f4e7550e25595db60420658e9d7463e3aa6bce48
TheDomesticUser <TheDomesticUser@gmail.com>
2020-04-05 22:55:08 -0400

    Added button response to clicks. Operator/numbers are now displayed

--- src/main.py
+++ src/main.py
@@ -2,27 +2,51 @@ import kivy
 kivy.require('1.11.1')
 
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.properties import ObjectProperty
 
 from kivy.core.window import Window
 
 class CalcDisplay(BoxLayout):
-    pass
+    resultsDsp = ObjectProperty(None)
+    
+    def addNum(self, num):
+        # append the num to results display
+        self.resultsDsp.text += str(num)
+
+    def addOperator(self, operator):
+        # append the operator to results display
+        self.resultsDsp.text += f' {operator} '
+
+    def addDecimal(self, decimal):
+        self.resultsDsp.text += str(decimal)
+
+    def deleteRightmostChar(self):
+        # trim possible spaces due to spaces between operators
+        self.resultsDsp.text = self.resultsDsp.text.rstrip()
+
+        # delete one character of results display
+        self.resultsDsp.text = self.resultsDsp.text[:-1]
+
+        self.resultsDsp.text = self.resultsDsp.text.rstrip()
+
+    def calcValue(self):
+        # parse the operators left to right
+        pass
 
 class ResultsDisplay(TextInput):
     pass
 
-class ButtonsDisplay(GridLayout):
+class ButtonsDisplay(ButtonBehavior, GridLayout):
     pass
 
 class CalcApp(App):
     def build(self):
         # set the window size
-        Window.size = (450, 450)
+        Window.size = (400, 450)
 
         calcDisplay = CalcDisplay()
 
